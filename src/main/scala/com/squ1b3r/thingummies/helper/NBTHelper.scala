09fd4c3e3b65ef10bfc8dcae538ea6ca9f7fe0c9
Andrey Kutyrev <andrey.kutyrev@gmail.com>
2014-12-05 15:25:21 +0500

    This should do...

--- src/main/scala/com/squ1b3r/thingummies/helper/NBTHelper.scala
+++ src/main/scala/com/squ1b3r/thingummies/helper/NBTHelper.scala
@@ -22,10 +22,10 @@ import net.minecraft.nbt.NBTTagCompound
 object NBTHelper {
 
   /**
-	 * Returns compound tag for itemStack if exists. Creates an empty one otherwise
-	 *
-	 * @param stack ItemStack to check tag existence for
-	 */
+   * Returns compound tag for itemStack if exists. Creates an empty one otherwise
+   *
+   * @param stack ItemStack to check tag existence for
+   */
   def getNBTTagCompound(stack: ItemStack): NBTTagCompound = {
     if (stack.stackTagCompound == null) {
       stack.stackTagCompound = new NBTTagCompound
@@ -34,44 +34,44 @@ object NBTHelper {
   }
 
   /**
-	 * Checks existence of the compound tag
-	 *
-	 * @param stack ItemStack to check key existence in
+   * Checks existence of the compound tag
+   *
+   * @param stack ItemStack to check key existence in
    * @param key Key to check existence for
-	 */
+   */
   def verifyNBTKey(stack: ItemStack, key: String): Boolean = getNBTTagCompound(stack).hasKey(key)
 
   /**
-	 * Reads Int value from NBT using given key
-	 *
-	 * @param stack stack to read data from
+   * Reads Int value from NBT using given key
+   *
+   * @param stack stack to read data from
    * @param key Key to read data for
-	 */
+   */
   def readIntegerFromNBT(stack: ItemStack, key: String): Int = getNBTTagCompound(stack).getInteger(key)
 
   /**
-	 * Reads String value from NBT using given key
-	 *
-	 *@param stack stack to read data from
+   * Reads String value from NBT using given key
+   *
+   * @param stack stack to read data from
    * @param key Key to read data for
-	 */
+   */
   def readStringFromNBT(stack: ItemStack, key: String): String = getNBTTagCompound(stack).getString(key)
 
   /**
-	 * Reads Boolean value from NBT using given key
-	 *
-	 * @param stack stack to read data from
+   * Reads Boolean value from NBT using given key
+   *
+   * @param stack stack to read data from
    * @param key Key to read data for
-	 */
+   */
   def readBooleanFromNBT(stack: ItemStack, key: String): Boolean = getNBTTagCompound(stack).getBoolean(key)
 
   /**
-	 * Writes given data to given ItemStack with given key
-	 *
-	 * @param stack stack to write data for
+   * Writes given data to given ItemStack with given key
+   *
+   * @param stack stack to write data for
    * @param key Key to write data with
    * @param value Value to write
-	 */
+   */
   def writeToNBT(stack: ItemStack, key: String, value: Any): ItemStack = {
     val compound = getNBTTagCompound(stack)
 
