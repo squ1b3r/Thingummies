3ae74bc53ad15d1021805ae714f659ad105d9777
Andrey Kutyrev <andrey.kutyrev@gmail.com>
2015-06-26 14:02:16 +0500

    Fix colors for AE2 facades

--- src/main/scala/squ1b3r/thingummies/blocks/ItemBlockFactory.scala
+++ src/main/scala/squ1b3r/thingummies/blocks/ItemBlockFactory.scala
@@ -17,8 +17,10 @@
 package squ1b3r.thingummies.blocks
 
 
+import cpw.mods.fml.relauncher.{Side, SideOnly}
 import net.minecraft.block.Block
 import net.minecraft.item.{ItemBlock, ItemStack}
+import net.minecraft.util.IIcon
 
 import squ1b3r.thingummies.helper.ColorHelper
 
@@ -26,7 +28,15 @@ class ItemBlockFactory(block: Block) extends ItemBlock(block) {
 
   setHasSubtypes(true)
 
-  override def getMetadata(damage: Int): Int = damage
+  override def getMetadata(meta: Int): Int = meta
+
+  override def getIconFromDamage(meta: Int): IIcon = block.getIcon(2, meta)
+
+  @SideOnly(Side.CLIENT)
+  override def getColorFromItemStack(stack: ItemStack, meta: Int): Int = stack.getItemDamage match {
+    case 0 | 15 => 16777215
+    case _ => ColorHelper.getColor(stack.getItemDamage)
+  }
 
   override def getUnlocalizedName(stack: ItemStack): String = {
     super.getUnlocalizedName + "." + ColorHelper.getColorName(stack.getItemDamage)
